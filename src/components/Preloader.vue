bbd1055a20c94f762fb8b6422f1f57409da67fec
Alex House <ahouse@umich.edu>
2017-07-31 10:33:56 -0400

    Added base styles and the importer from Shoebox.

---
+++ src/components/Preloader.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="preloader">
+		<div class="spinner">Loading...</div>
+		<div class="preloader-message">{{ message }}</div>
+	</div>
+</template>
+
+<script>
+module.exports = {
+	name: 'preloader',
+	props: {
+		message: {
+			type: String,
+			default: ''
+		}
+	},
+	data: () => {
+		return {};
+	},
+	computed: {
+		
+	},
+	methods: {
+		
+	},
+	created: function() {
+		
+	}
+}
+</script>
+
+<style>
+/* Styles in global stylesheet. */
+</style>
